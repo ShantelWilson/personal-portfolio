0c2b3e5d4fce3b3accdd7dee6082cdd1e8c6d7cf
ShantelWilson <nicoleb3737@gmail.com>
2022-08-13 11:10:46 -0500

    changed tracker

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -22,8 +22,7 @@ export const Footer = () => {
                         
                         </div>
                         <p>Copyright 2022. Shantel Wilson All Rights Reserved</p>
-                        <a href="https://hits.seeyoufarm.com"><img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fgjbae1212%2Fhit-counter&count_bg=%23983ACF&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false"/></a>
-                    </Col>
+                        <a href="https://hits.seeyoufarm.com"><img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fshantelwilson.github.io%2Fpersonal-portfolio%2F&count_bg=%23983ACF&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false"/></a>                    </Col>
                 </Row>
 
             </Container>
